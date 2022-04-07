c2eb10ca0296f048d8f8662133cc60b68c0fa203
entirelymagic <entirelymagic@gmail.com>
2022-04-07 13:27:41 +0300

    fix db test issue

--- server/taxi/settings/testing.py
+++ server/taxi/settings/testing.py
@@ -2,7 +2,7 @@ from .local import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
