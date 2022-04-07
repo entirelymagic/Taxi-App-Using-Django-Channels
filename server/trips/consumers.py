8d0d85adc1308a1803518f19eb8b8107e9e14b09
entirelymagic <entirelymagic@gmail.com>
2022-04-07 15:43:06 +0300

    create trip, add the coresponding rider to it and send the trip to the driver group

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -1,35 +1,84 @@
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+from trips.models import Trip
+from trips.serializers import NestedTripSerializer, TripSerializer
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
     groups = ['test']
+
+    @database_sync_to_async
+    def _get_user_group(self, user):
+        return user.groups.first().name
+
+    @database_sync_to_async
+    def _create_trip(self, data):
+        serializer = TripSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        return serializer.create(serializer.validated_data)
+
+    @database_sync_to_async
+    def _get_trip_data(self, trip):
+        return NestedTripSerializer(trip).data
+
+    @database_sync_to_async
+    def _get_trip_ids(self, user):
+        user_groups = user.groups.values_list('name', flat=True)
+        if 'driver' in user_groups:
+            trip_ids = user.trips_as_driver.exclude(status=Trip.COMPLETED).only('id').values_list('id', flat=True)
+        else:
+            trip_ids = user.trips_as_rider.exclude(status=Trip.COMPLETED).only('id').values_list('id', flat=True)
+        return map(str, trip_ids)
 
     async def connect(self):
         user = self.scope['user']
         if user.is_anonymous:
             await self.close()
         else:
-            await self.channel_layer.group_add(group='test', channel=self.channel_name)
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_add(group='drivers', channel=self.channel_name)
+
+            for trip_id in await self._get_trip_ids(user):
+                await self.channel_layer.group_add(group=trip_id, channel=self.channel_name)
             await self.accept()
 
+    async def create_trip(self, message):
+        data = message.get('data')
+        trip = await self._create_trip(data)
+        trip_data = await self._get_trip_data(trip)
+
+        # Send rider requests to all drivers.
+        await self.channel_layer.group_send(
+            group='drivers',
+            message={'type': 'echo.message', 'data': trip_data},
+        )
+
+        # Add rider to trip group.
+        await self.channel_layer.group_add(group=f'{trip.id}', channel=self.channel_name)
+
+        await self.send_json({'type': 'echo.message', 'data': trip_data})
+
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(group='test', channel=self.channel_name)
+        user = self.scope['user']
+        if user.is_anonymous:
+            await self.close()
+        else:
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_discard(group='drivers', channel=self.channel_name)
+
+            for trip_id in await self._get_trip_ids(user):
+                await self.channel_layer.group_discard(group=trip_id, channel=self.channel_name)
         await super().disconnect(code)
 
     async def echo_message(self, message):
-        await self.send_json(
-            {
-                'type': message.get('type'),
-                'data': message.get('data'),
-            }
-        )
+        await self.send_json(message)
 
     async def receive_json(self, content, **kwargs):
         message_type = content.get('type')
-        if message_type == 'echo.message':
-            await self.send_json(
-                {
-                    'type': message_type,
-                    'data': content.get('data'),
-                }
-            )
+        if message_type == 'create.trip':
+            await self.create_trip(content)
+        elif message_type == 'echo.message':
+            await self.echo_message(content)
--- server/trips/models.py
+++ server/trips/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.shortcuts import reverse
@@ -27,6 +28,12 @@ class Trip(models.Model):
     pick_up_address = models.CharField(max_length=255)
     drop_off_address = models.CharField(max_length=255)
     status = models.CharField(max_length=20, choices=STATUSES, default=REQUESTED)
+    driver = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.DO_NOTHING, related_name='trips_as_driver'
+    )
+    rider = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.DO_NOTHING, related_name='trips_as_rider'
+    )
 
     def __str__(self):
         return f'{self.id}'
