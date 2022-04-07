2662aae0b291e9d58e8d9a79e7e353c974b2b1eb
entirelymagic <entirelymagic@gmail.com>
2022-04-07 14:34:59 +0300

    basic Django Channel part 1

--- server/taxi/routing.py
+++ server/taxi/routing.py
@@ -1,8 +1,19 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from trips.consumers import TaxiConsumer
+
+from taxi.middleware import TokenAuthMiddlewareStack
 
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
+        'websocket': TokenAuthMiddlewareStack(
+            URLRouter(
+                [
+                    path('taxi/', TaxiConsumer.as_asgi()),
+                ]
+            )
+        ),
     }
 )
