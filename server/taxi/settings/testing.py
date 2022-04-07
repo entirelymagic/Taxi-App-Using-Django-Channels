a66cb5e3d7b7a5fc711a992308058c43d7d9d127
entirelymagic <entirelymagic@gmail.com>
2022-04-07 13:07:14 +0300

    try to fix port number Error

--- server/taxi/settings/testing.py
+++ server/taxi/settings/testing.py
@@ -3,8 +3,8 @@ from .local import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('PGDATABASE'),
-        'USER': os.getenv('PGUSER'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('PGPASSWORD'),
         'HOST': os.getenv('PGHOST', 'localhost'),
         'PORT': os.getenv('PGPORT', '5432'),
